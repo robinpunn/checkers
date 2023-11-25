14ee91b6820b38136ddb785595e51af6788e0774
robin <robin.punnoose@protonmail.com>
2023-11-25 15:55:07 -0600

    added move count feature

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -27,59 +27,67 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "1", // TODO: update with a proper value when updated
+		GameIndex: "1",
 	}, *createResponse)
 }
 
 func TestCreate1GameHasSaved(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
-	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	systemInfo, found := keeper.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{
 		NextId: 2,
 	}, systemInfo)
-	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:     "1",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     bob,
+		Red:       carol,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, game1)
 }
 
 func TestCreate1GameGetAll(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
-	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	games := keeper.GetAllStoredGame(ctx)
 	require.Len(t, games, 1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:     "1",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     bob,
+		Red:       carol,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, games[0])
 }
 
 func TestCreate1GameEmitted(t *testing.T) {
 	msgSrvr, _, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
-	ctx := sdk.UnwrapSDKContext(context)
 	require.NotNil(t, ctx)
 	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
 	require.Len(t, events, 1)
@@ -93,6 +101,32 @@ func TestCreate1GameEmitted(t *testing.T) {
 			{Key: "red", Value: carol},
 		},
 	}, event)
+}
+
+func TestCreateGameRedAddressBad(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     "notanaddress",
+	})
+	require.Nil(t, createResponse)
+	require.Equal(t,
+		"red address is invalid: notanaddress: decoding bech32 failed: invalid separator index -1",
+		err.Error())
+}
+
+func TestCreateGameEmptyRedAddress(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     "",
+	})
+	require.Nil(t, createResponse)
+	require.Equal(t,
+		"red address is invalid: : empty address string is not allowed",
+		err.Error())
 }
 
 func TestCreate3Games(t *testing.T) {
@@ -122,8 +156,9 @@ func TestCreate3Games(t *testing.T) {
 	}, *createResponse3)
 }
 
-func TestCreate3GamesGetAll(t *testing.T) {
+func TestCreate3GamesHasSaved(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -139,28 +174,98 @@ func TestCreate3GamesGetAll(t *testing.T) {
 		Black:   alice,
 		Red:     bob,
 	})
-	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 4,
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index:     "1",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     bob,
+		Red:       carol,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
+	}, game1)
+	game2, found2 := keeper.GetStoredGame(ctx, "2")
+	require.True(t, found2)
+	require.EqualValues(t, types.StoredGame{
+		Index:     "2",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     carol,
+		Red:       alice,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
+	}, game2)
+	game3, found3 := keeper.GetStoredGame(ctx, "3")
+	require.True(t, found3)
+	require.EqualValues(t, types.StoredGame{
+		Index:     "3",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     alice,
+		Red:       bob,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
+	}, game3)
+}
+
+func TestCreate3GamesGetAll(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Black:   alice,
+		Red:     bob,
+	})
+	games := keeper.GetAllStoredGame(ctx)
 	require.Len(t, games, 3)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:     "1",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     bob,
+		Red:       carol,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, games[0])
 	require.EqualValues(t, types.StoredGame{
-		Index: "2",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: carol,
-		Red:   alice,
+		Index:     "2",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     carol,
+		Red:       alice,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, games[1])
 	require.EqualValues(t, types.StoredGame{
-		Index: "3",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: alice,
-		Red:   bob,
+		Index:     "3",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     alice,
+		Red:       bob,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, games[2])
 }
 
@@ -188,38 +293,15 @@ func TestCreateGameFarFuture(t *testing.T) {
 	game1, found1 := keeper.GetStoredGame(ctx, "1024")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1024",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:     "1024",
+		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Black:     bob,
+		Red:       carol,
+		Winner:    "*",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+		MoveCount: 0,
 	}, game1)
-}
-
-func TestCreateGameRedAddressBad(t *testing.T) {
-	msgServer, _, context := setupMsgServerCreateGame(t)
-	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: alice,
-		Black:   bob,
-		Red:     "notanaddress",
-	})
-	require.Nil(t, createResponse)
-	require.Equal(t,
-		"red address is invalid: notanaddress: decoding bech32 failed: invalid separator index -1",
-		err.Error())
-}
-
-func TestCreateGameEmptyRedAddress(t *testing.T) {
-	msgServer, _, context := setupMsgServerCreateGame(t)
-	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: alice,
-		Black:   bob,
-		Red:     "",
-	})
-	require.Nil(t, createResponse)
-	require.Equal(t,
-		"red address is invalid: : empty address string is not allowed",
-		err.Error())
 }
 
 func TestSavedCreatedDeadlineIsParseable(t *testing.T) {
