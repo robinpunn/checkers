090b4a4b5addfbe203ef2f5d5e98b617433124ac
robin <robin.punnoose@protonmail.com>
2023-11-28 14:13:02 -0600

    added TestMsgServer to mes_server_test

--- x/checkers/keeper/msg_server_test.go
+++ x/checkers/keeper/msg_server_test.go
@@ -8,9 +8,16 @@ import (
 	keepertest "github.com/robinpunn/checkers/testutil/keeper"
 	"github.com/robinpunn/checkers/x/checkers/keeper"
 	"github.com/robinpunn/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	k, ctx := keepertest.CheckersKeeper(t)
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	msgServer, context := setupMsgServer(t)
+	require.NotNil(t, msgServer)
+	require.NotNil(t, context)
 }
